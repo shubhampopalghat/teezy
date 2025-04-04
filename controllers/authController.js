658ceeaf8c251ae83c9b85c73ae1c7a38874f742
Shubh <shubhampopalghat77@gmail.com>
2025-04-05 02:16:44 +0530

    ready for deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/user-model");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { generateToken } = require('../utils/generateToken');
 
 module.exports.registerUser =  async (req, res) => {
